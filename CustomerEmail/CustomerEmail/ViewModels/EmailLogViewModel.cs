810446da7edff3ed03b4d9a72033cb079c760d52
Rgoldn589 <Goldner.Ronald@gmail.com>
2025-05-04 17:55:19 -0400

    Add files via upload

---
+++ CustomerEmail/CustomerEmail/ViewModels/EmailLogViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CustomerEmail.Helper;
+using CustomerEmail.Models;
+using CustomerEmail.Views;
+using System.Configuration;
+using System.IO;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace CustomerEmail.ViewModels
+{
+    public class EmailLogViewModel
+    {
+
+        public EmailLogView Window { get; set; }
+
+        public List<EmailLog> Log { get; set; } = new List<EmailLog>();
+
+        private RelayCommand _close;
+        public RelayCommand Close
+        {
+            get
+            {
+                if (_close == null)
+                {
+                    _close = new RelayCommand(CloseForm, true);
+                }
+
+                return _close;
+            }
+        }
+
+        public EmailLogViewModel()
+        {
+            GetData();
+        }
+
+        public void GetData()
+        {
+            var path = Path.Combine(ConfigurationManager.AppSettings["LoggingPath"],
+                    ConfigurationManager.AppSettings["EmailLogFile"]);
+
+
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            var log = File.ReadAllLines(path);
+
+            foreach (var logItem in log)
+            {
+                string[] line = logItem.Split(",");
+
+                Log.Add(new EmailLog()
+                {
+                    Email = line[0],
+                    Subject = line[1],
+                    Message = line[2],
+                    LogDate = DateTime.Parse(line[3]),
+                    Result = line[4]          
+                });
+            }
+        }
+
+        private void CloseForm()
+        {
+            Window.Close();
+        }
+    }
+}
---
+++ CustomerEmail/EmailSend/SendEmailTransimission.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Mail;
+using EmailSend.Helper;
+using EmailSend.Models;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace EmailSend
+{
+    public class SendEmailTransimission
+    {
+        private readonly IConfiguration _config;
+
+        public SendEmailTransimission()
+        {
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Make sure this is the root directory
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            _config = config;
+
+        }
+
+        public async Task SendEmailAsync(EmailValues email)
+        {
+            var attempts = 0;
+            var success = false;
+            var port = Int32.Parse(_config["EmailPort"]);
+
+            while (!success && attempts < HelperValues.MaxEmailAttempts)
+            {
+                try
+                {
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress(_config["ClientName"], _config["ClientEmailAddress"]));
+                    message.To.Add(new MailboxAddress(email.Name, email.EmailAddress));
+                    message.Subject = email.Subject;
+                    message.Body = new TextPart("plain") { Text = email.Message };
+
+                    using (var client = new MailKit.Net.Smtp.SmtpClient())
+                    {
+                        await client.ConnectAsync(_config["EmailHost"], port);
+                        await client.AuthenticateAsync(_config["ClientEmailAddress"], _config["EmailPassword"]);
+                        await client.SendAsync(message);
+                        await client.DisconnectAsync(true);
+                    }
+
+                    success = true;
+                    WriteEmailLog(success, email);
+                }
+                catch (SmtpFailedRecipientsException ex)
+                {
+                    foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+                    {
+                        Console.WriteLine($"Failed to deliver message to {t.FailedRecipient}: {t.Message}");
+                    }
+
+                    WriteEmailLog(false, email);
+                    attempts++;
+                }
+                catch (SmtpException ex)
+                {
+                    Console.WriteLine($"SMTP error occurred: {ex.Message}");
+                    WriteEmailLog(success, email);
+                    attempts++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"SMTP error occurred: {ex.Message}");
+                    WriteEmailLog(success, email);
+                    attempts++;
+                }
+            }
+        }   
+
+        private async void WriteEmailLog(bool success, EmailValues email)
+        {
+            var status = success ? "Sent" : "Failed";
+
+            if (!Directory.Exists(_config["LoggingPath"]))
+            {
+                Directory.CreateDirectory(_config["LoggingPath"]);
+            }
+
+            string line = email.EmailAddress + ", " + email.Subject + ", " + email.Message + "," + DateTime.Now.ToString() + ", " + status + "\n";
+            await File.AppendAllTextAsync(Path.Combine(_config["LoggingPath"], _config["EmailLogFile"]), line);
+
+        }
+    }
+}
